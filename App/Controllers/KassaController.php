94fdfdd281543587406f31381766a5ed2346ad68
Xar0n <dima.dima.maksimov1998@mail.ru>
2021-05-01 12:32:51 +0800

    actions add delete

--- App/Controllers/KassaController.php
+++ App/Controllers/KassaController.php
@@ -5,6 +5,10 @@ namespace App\Controllers;
 
 
 use App\Controller;
+use App\Exceptions\Http\Http404Exception;
+
+use App\Exceptions\Model\ItemNotFoundException;
+use App\Models\Kassa;
 use App\Views\Twig;
 
 class KassaController extends Controller
@@ -16,26 +20,59 @@ class KassaController extends Controller
 
     protected function actionIndex()
     {
-        $this->view->display('kassa/index.twig', []);
+        $kassas = Kassa::findAll();
+        $this->view->display('kassa/index.twig', ['kassas' => $kassas]);
     }
 
     protected function actionAdd()
     {
-        $this->view->display('kassa/index.twig', []);
+        if (isset($_POST['save'])) {
+            $kassa = new Kassa();
+            $kassa->naselennyj_punkt = filter_input(INPUT_POST, 'naselennyj_punkt', FILTER_SANITIZE_STRING);
+            $kassa->ulica = filter_input(INPUT_POST, 'ulica', FILTER_SANITIZE_STRING);
+            $kassa->nomer_doma = filter_input(INPUT_POST, 'nomer_doma', FILTER_SANITIZE_STRING);
+            $kassa->save();
+        }
+        $this->view->display('kassa/add.twig', []);
     }
 
     protected function actionEdit()
     {
-        $this->view->display('kassa/index.twig', []);
-    }
-
-    protected function actionSave()
-    {
-        $this->view->display('kassa/index.twig', []);
+        try {
+            $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+            if (empty($id)) {
+                throw new \InvalidArgumentException;
+            }
+            $kassa = Kassa::findById($id);
+        } catch (ItemNotFoundException $e) {
+            throw new Http404Exception;
+        }
+        if (isset($_POST['save'])) {
+            $kassa->naselennyj_punkt = filter_input(INPUT_POST, 'naselennyj_punkt', FILTER_SANITIZE_STRING);
+            $kassa->ulica = filter_input(INPUT_POST, 'ulica', FILTER_SANITIZE_STRING);
+            $kassa->nomer_doma = filter_input(INPUT_POST, 'nomer_doma', FILTER_SANITIZE_STRING);
+            $kassa->save();
+            header('Location:/kassa/');
+            exit();
+        }
+        $this->view->display('kassa/edit.twig', ['kassa' => $kassa]);
     }
 
     protected function actionDelete()
     {
-        $this->view->display('kassa/index.twig', []);
+        try {
+            $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+            if (empty($id)) {
+                throw new \InvalidArgumentException;
+            }
+            $kassa = Kassa::findById($id);
+            $kassa->delete();
+            header('Location:/kassa/');
+            exit();
+        } catch (ItemNotFoundException $e) {
+            throw new Http404Exception;
+        } catch (\InvalidArgumentException $e) {
+            throw new Http415Exception;
+        }
     }
 }
