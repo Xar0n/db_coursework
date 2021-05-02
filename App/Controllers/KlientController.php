cfdf954c319baa7d47cebbca7339fff739d36ebe
Xar0n <dima.dima.maksimov1998@mail.ru>
2021-05-02 12:50:58 +0800

    page klient

--- App/Controllers/KlientController.php
+++ App/Controllers/KlientController.php
@@ -2,11 +2,68 @@
 
 
 namespace App\Controllers;
-
-
 use App\ControllerTwig;
+use App\Models\Klient;
 
 class KlientController extends ControllerTwig
 {
+    protected function actionIndex()
+    {
+        $klient = Klient::findAll();
+        $this->view->display('klient/index.twig', ['klients' => $klient]);
+    }
 
+    protected function actionAdd()
+    {
+        if (isset($_POST['save'])) {
+            $klient = new Klient();
+            $klient->nomer_i_seriya_pasporta = filter_input(INPUT_POST, 'nomer_i_seriya_pasporta', FILTER_SANITIZE_STRING);
+            $klient->familiya = filter_input(INPUT_POST, 'familiya', FILTER_SANITIZE_STRING);
+            $klient->imya = filter_input(INPUT_POST, 'imya', FILTER_SANITIZE_STRING);
+            $klient->otchestvo = filter_input(INPUT_POST, 'otchestvo', FILTER_SANITIZE_STRING);
+            $klient->save();
+        }
+        $this->view->display('klient/add.twig', []);
+    }
+
+    protected function actionEdit()
+    {
+        try {
+            $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+            if (empty($id)) {
+                throw new \InvalidArgumentException;
+            }
+            $klient = Klient::findById($id);
+        } catch (ItemNotFoundException $e) {
+            throw new Http404Exception;
+        }
+        if (isset($_POST['save'])) {
+            $klient->nomer_i_seriya_pasporta = filter_input(INPUT_POST, 'nomer_i_seriya_pasporta', FILTER_SANITIZE_STRING);
+            $klient->familiya = filter_input(INPUT_POST, 'familiya', FILTER_SANITIZE_STRING);
+            $klient->imya = filter_input(INPUT_POST, 'imya', FILTER_SANITIZE_STRING);
+            $klient->otchestvo = filter_input(INPUT_POST, 'otchestvo', FILTER_SANITIZE_STRING);
+            $klient->save();
+            header('Location:/klient/');
+            exit();
+        }
+        $this->view->display('klient/edit.twig', ['klient'=>$klient]);
+    }
+
+    protected function actionDelete()
+    {
+        try {
+            $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+            if (empty($id)) {
+                throw new \InvalidArgumentException;
+            }
+            $klient = Klient::findById($id);
+            $klient->delete();
+            header('Location:/klient/');
+            exit();
+        } catch (ItemNotFoundException $e) {
+            throw new Http404Exception;
+        } catch (\InvalidArgumentException $e) {
+            throw new Http415Exception;
+        }
+    }
 }
--- App/Models/Klient.php
+++ App/Models/Klient.php
@@ -9,6 +9,7 @@ use App\Model;
 class Klient extends Model
 {
     protected static $table = 'klient';
+    public $nomer_i_seriya_pasporta;
     public $familiya;
     public $imya;
     public $otchestvo;
