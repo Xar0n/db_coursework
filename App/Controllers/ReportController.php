c4bb1a20932e926e52908105195bfdced0a400fa
Xar0n <dima.dima.maksimov1998@mail.ru>
2021-05-06 11:18:28 +0800

    new report and fix mistakes

--- App/Controllers/ReportController.php
+++ App/Controllers/ReportController.php
@@ -5,8 +5,8 @@ namespace App\Controllers;
 
 
 use App\ControllerTwig;
+use App\Models\Aviakompaniya;
 use App\Requests\Report;
-use OzdemirBurak\JsonCsv\AbstractFile;
 use OzdemirBurak\JsonCsv\File\Json;
 
 class ReportController extends ControllerTwig
@@ -20,18 +20,18 @@ class ReportController extends ControllerTwig
     {
         $clients = [];
         $nope = false;
-        if (isset($_POST['save'])) {
+        if (isset($_POST['show'])) {
             $data_prodazhi = filter_input(INPUT_POST, 'data_prodazhi', FILTER_SANITIZE_STRING);
             $clients = Report::listClientsOnDate($data_prodazhi);
             if(empty($clients)) $nope = true;
         } elseif (isset($_POST['report'])) {
-
             $data_prodazhi = filter_input(INPUT_POST, 'data_prodazhi', FILTER_SANITIZE_STRING);
             $clients = Report::listClientsOnDate($data_prodazhi);
             $json = json_encode($clients);
-            file_put_contents(__DIR__ . '/../../public/reports/reportgit.json', $json);
-            $jsonConvert = new Json(__DIR__ . '/../../public/reports/report.json');
+            file_put_contents(__DIR__ . '/../../public/reports/report2.json', $json);
+            $jsonConvert = new Json(__DIR__ . '/../../public/reports/report2.json');
             $jsonConvert->setConversionKey('utf8_encoding', true);
+            $jsonConvert->setConversionKey('delimiter', ';');
             $jsonConvert->convertAndDownload();
         }
         $this->view->display('report/listClientsOnDate.twig', ['clients' => $clients, 'nope' => $nope]);
@@ -39,6 +39,17 @@ class ReportController extends ControllerTwig
 
     protected function actionSalesMonthSelectCompany()
     {
-        debug(Report::salesMonthSelectCompany());
+        $nope = false;
+        $bilets = [];
+        if (isset($_POST['show'])) {
+            $shifr_aviakompanii = filter_input(INPUT_POST, 'shifr_aviakompanii', FILTER_SANITIZE_NUMBER_INT);
+            $month = filter_input(INPUT_POST, 'month', FILTER_SANITIZE_NUMBER_INT);
+            $bilets = Report::salesMonthSelectCompany($shifr_aviakompanii, $month);
+            if(empty($bilets)) $nope = true;
+        } elseif (isset($_POST['report'])) {
+
+        }
+        $aviakompaniyas = Aviakompaniya::findAll();
+        $this->view->display('report/salesMonthSelectCompany.twig', ['aviakompaniyas' => $aviakompaniyas, 'nope' => $nope, 'bilets' => $bilets]);
     }
 }
--- App/Requests/Report.php
+++ App/Requests/Report.php
@@ -17,13 +17,19 @@ class Report extends Request
         kupon.nunkt_vysadki, bilet.tip, bilet.data_prodazhi, bilet.shifr_aviakompanii, aviakompaniya.nazvanie, aviakompaniya.naselennyj_punkt, 
         aviakompaniya.ulica, aviakompaniya.nomer_doma, aviakompaniya.ofis FROM `klient`, `kupon`, `bilet`, `aviakompaniya` 
         WHERE bilet.id = kupon.nomer_bileta AND klient.nomer_i_seriya_pasporta = kupon.nomer_i_seriya_pasporta_klienta 
-        and bilet.shifr_aviakompanii = aviakompaniya.id and bilet.data_prodazhi =  '."'".$date."'";
+        and bilet.shifr_aviakompanii = aviakompaniya.id and bilet.data_prodazhi =  \''.$date.'\'';
         return self::request($sql);
     }
 
-    public static function salesMonthSelectCompany()
+    public static function salesMonthSelectCompany($shifr_aviakompanii, $month)
     {
-        return 0;
+        $sql = 'SELECT bilet.*, aviakompaniya.nazvanie AS nazvanie_a, aviakompaniya.naselennyj_punkt AS naselennyj_punkt_a, 
+       aviakompaniya.ulica AS ulica_a, aviakompaniya.nomer_doma AS nomer_doma_a, aviakompaniya.ofis AS ofis_a, 
+       kassa.naselennyj_punkt, kassa.ulica, kassa.nomer_doma, kassir.familiya, kassir.imya, kassir.otchestvo FROM `bilet`, 
+       `aviakompaniya`, `kassa`, `kassir` WHERE month(`data_prodazhi`) = month(\'2020-'.$month.'-4\') 
+       AND `shifr_aviakompanii` = '.$shifr_aviakompanii.' AND bilet.shifr_aviakompanii = aviakompaniya.id AND bilet.nomer_kassy = kassa.id 
+                                          AND bilet.tabelnyj_nomer_kassira = kassir.id';
+        return self::request($sql);
     }
 
 
